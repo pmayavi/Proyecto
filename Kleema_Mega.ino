54044160b7376a3504445cd935018a8627cf8f7a
pmayavi <68768011+pmayavi@users.noreply.github.com>
2021-11-12 09:51:50 -0500

    Add files via upload

--- Kleema_Mega.ino
+++ Kleema_Mega.ino
@@ -1,7 +1,7 @@
 static int TEMP = A0;
-static int HUM = A1;
-static int PLUV = A2;
-static int SOL = A3;
+static int PLUV = A1;
+static int HUM = A3;
+static int SOL = A5;
 
 double temp = 0;//Grados centrigrados
 double hum = 0;//Porcentaje de humedad
@@ -13,6 +13,9 @@ void setup() {
   pinMode(HUM, INPUT);
   pinMode(PLUV, INPUT);
   pinMode(SOL, INPUT);
+
+  pinMode(52, OUTPUT);
+  digitalWrite(52, HIGH);
 
   Serial.begin(9600);
 }
@@ -27,13 +30,13 @@ void loop() {
 
 //Colectar la data
 void data() {
-  temp = (((analogRead(TEMP) * 5.0 / 1023.0 * 1000.0) - 100) / 10) - 40;
+  temp = (((analogRead(TEMP) * 5.0 / 1023.0 * 1000.0) - 100) / 10) + 5;
   delay(5);
 
-  hum = analogRead(HUM) / 10.23;
+  hum = 95 - analogRead(HUM) / 10.23;
   delay(5);
 
-  pluv = analogRead(PLUV) / 10.23 / 2.0;
+  pluv = digitalRead(PLUV);
   delay(5);
 
   sol = analogRead(SOL) / 10.23 / 5.0;
@@ -42,34 +45,34 @@ void data() {
 
 //Buscar anomalias
 void alert() {
-  if (temp < 18)
-    Serial.println("ADVERTENCIA LA TEMPERATURA ESTA BAJA");
-  else if (temp > 25)
-    Serial.println("ADVERTENCIA LA TEMPERATURA ESTA ALTA");
+  alert(temp, 15, 25, "TEMPERATURA");
+  alert(sol, 3, 12, "LUZ");
 
-  if (hum < 40)
-    Serial.println("ADVERTENCIA LA HUMEDAD ESTA BAJA");
-  else if (hum > 60)
-    Serial.println("ADVERTENCIA LA HUMEDAD ESTA ALTA");
-
-  if (pluv < 3)
-    Serial.println("ADVERTENCIA NO HUBO LLUVIA");
-  else if (pluv > 30)
-    Serial.println("ADVERTENCIA LLUVIA ALTA");
-
-  if (sol < 5)
-    Serial.println("ADVERTENCIA SE PRESENTO POCAS HORAS DE SOL");
-  else if (sol > 12)
-    Serial.println("ADVERTENCIA SE PRESENTO MUCHAS HORAS DE SOL");
+  if(pluv)
+    sendData("__[ESTA LLOVIENDO!]__");
+  else
+    alert(hum, 30, 60, "HUMEDAD");
 }
 
+void alert(int sensor, int bajo, int alto, String tipo) {
+  if (sensor < bajo) {
+    sendData("__[" + tipo + " BAJA!]__");
+    Serial.println();
+    delay(500);
+  }
+  else if (sensor > alto) {
+    sendData("__[" + tipo + " ALTA!]__");
+    Serial.println();
+    delay(500);
+  }
+
+}
 
 //Enviar la data
 void sendData() {
-  sendData(".GET /temp="+ String(temp) + " HTTP/1,, ");
-  sendData(".GET /hum="+ String(hum) + " HTTP/1,, ");
-  sendData(".GET /pluv="+ String(pluv) + " HTTP/1,, ");
-  sendData(".GET /sol="+ String(sol) + " HTTP/1,, ");
+  sendData("__[Datos_temp=" + String(temp) + "hum=" + String(hum) + "pluv=" + String(pluv) + "sol=" + String(sol) + "Fin]__");
+  Serial.println();
+  delay(500);
 }
 
 void sendData(String peticion) {
--- Kleema_NodeMCU.ino
+++ Kleema_NodeMCU.ino
@@ -14,36 +14,43 @@ double Hum = 0;
 double Pluv = 0;
 double Sol = 0;
 char token[] = "GPbFO9dAP-pBMMEe0kpaUTDRdoECWa7W";
-BlynkTimer timer;
+//BlynkTimer timer;
+WidgetLCD lcd(V4);
 
-void setup(){
+void setup() {
   Serial.begin(9600);
-  timer.setInterval(2000L, sendInfo); //Cada 2 mil mili llamar la funcion sensor
-  Blynk.begin(token, "Electronica", "ST0248-001");
+  //timer.setInterval(2000L, sendInfo); //Cada 2 mil mili llamar la funcion sensor
+  Blynk.begin(token, "Salomon", "marianuwu");
 }
 
 void loop() {
   readSerial();
-  if (wor.indexOf("ET /") != -1) {
-    if (wor.indexOf("temp") != -1) {
-      Temp = wor.substring(wor.indexOf("temp=") + 1, wor.length() - 7).toDouble();
-    }  if (wor.indexOf("hum") != -1) {
-      Hum = wor.substring(wor.indexOf("hum=") + 1, wor.length() - 7).toDouble();
-    }  if (wor.indexOf("pluv") != -1) {
-      Pluv = wor.substring(wor.indexOf("pluv=") + 1, wor.length() - 7).toDouble();
-    } if (wor.indexOf("sol") != -1) {
-      Sol = wor.substring(wor.indexOf("sol=") + 1, wor.length() - 7).toDouble();
-    }
-  }
+  interpretar();
+  sendInfo();
+  alert();
   wor = "";
-  delay(100);
+  delay(5);
+}
+
+void interpretar(){
+  Serial.println(wor);
+  if (wor.indexOf("Datos") != -1) {
+    if(wor.indexOf("temp=") != -1)
+      Temp = wor.substring(wor.indexOf("temp=") + 5, wor.indexOf("hum=")).toDouble();
+    if(wor.indexOf("hum=") != -1)
+      Hum = wor.substring(wor.indexOf("hum=") + 4, wor.indexOf("pluv=")).toDouble();
+    if(wor.indexOf("pluv=") != -1)
+      Pluv = wor.substring(wor.indexOf("pluv=") + 5, wor.indexOf("sol=")).toDouble();
+    if(wor.indexOf("sol=") != -1)
+      Sol = wor.substring(wor.indexOf("sol=") + 4, wor.indexOf("Fin")).toDouble();
+  }
 }
 
 void readSerial() {
   if (Serial.available()) {
     cha = Serial.read();
-    if (cha = '/') {
-      while (Serial.available() && cha != ',') {
+    if (cha = '[') {
+      while (Serial.available() && cha != ']') {
         wor += cha;
         cha = Serial.read();
       }
@@ -56,4 +63,12 @@ void sendInfo() {
   Blynk.virtualWrite(V1, Hum);
   Blynk.virtualWrite(V2, Pluv);
   Blynk.virtualWrite(V3, Sol);
+}
+
+void alert(){
+  if (wor.indexOf("!") != -1){
+    wor.replace("[","");
+    wor.replace("_","");
+    lcd.print(0,0,wor);
+  } else lcd.clear();
 }
